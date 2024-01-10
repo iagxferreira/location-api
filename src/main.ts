2eab30684d14f29d290f0abba9e4ed2a027d86e5
Iago Ferreira <iago-ferreira@outlook.com>
2024-01-09 21:32:58 -0300

    chore: dockerizing application and adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Location API')
+    .setDescription('Technical challenge')
+    .setVersion('1.0')
+    .addTag('location')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.APP_PORT || 3000;
+  Logger.log(`[Application] running on ${PORT} port.`);
+  await app.listen(PORT);
 }
 bootstrap();
